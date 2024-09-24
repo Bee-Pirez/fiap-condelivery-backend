550e849cd7b7fec0d3ca55e9e0bd2d4ffb95a3da
Beatriz <beatrizcarmoperez@gmail.com>
2024-09-24 17:44:23 -0300

    feat: add assets

---
+++ user/src/main/java/br/com/condelivery/user/model/Address.java
@@ -0,0 +1,35 @@
+package br.com.condelivery.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "address")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "street", nullable = false)
+    private String street;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "state", nullable = false)
+    private String state;
+
+    @Column(name = "zip_code", nullable = false)
+    private String zipCode;
+
+    @Column(name = "number", nullable = false)
+    private String number;
+}
