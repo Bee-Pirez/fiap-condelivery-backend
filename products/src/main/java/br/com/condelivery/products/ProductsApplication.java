ab7dc9c73ead6ad840dfed9cb9a4bf8c0cffd5ad
Beatriz <beatrizcarmoperez@gmail.com>
2024-09-30 02:48:08 -0300

    chore: update project configurations and application properties for products and user modules

--- products/src/main/java/br/com/condelivery/products/ProductsApplication.java
+++ products/src/main/java/br/com/condelivery/products/ProductsApplication.java
@@ -1,12 +1,22 @@
 package br.com.condelivery.products;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductsApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		String myEnvVar2 = dotenv.get("SPRING_DATASOURCE_USERNAME");
+		String myEnvVar3 = dotenv.get("SPRING_DATASOURCE_PASSWORD");
+
+
+
 		SpringApplication.run(ProductsApplication.class, args);
 	}
 
--- user/src/main/java/br/com/condelivery/user/UserApplication.java
+++ user/src/main/java/br/com/condelivery/user/UserApplication.java
@@ -1,12 +1,22 @@
 package br.com.condelivery.user;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		String myEnvVar2 = dotenv.get("SPRING_DATASOURCE_USERNAME");
+		String myEnvVar3 = dotenv.get("SPRING_DATASOURCE_PASSWORD");
+
+
+
 		SpringApplication.run(UserApplication.class, args);
 	}
 
